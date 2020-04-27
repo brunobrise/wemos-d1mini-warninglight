0bd8634470f94ebdc4d21e396e1dc1c88aeedc75
Bruno Brito Semedo <code@brunobrise.com>
2020-04-27 02:16:10 +0200

    show wifi connection info (web ui)

--- wemos-d1mini-warninglight.ino
+++ wemos-d1mini-warninglight.ino
@@ -65,9 +65,13 @@ const char index_html[] PROGMEM = R"rawliteral(
     </div>
     <div class="flex flex-wrap justify-center mt-4">
       <h1 class="text-xl font-bold mb-2 text-gray-900">SYSTEM INFORMATION</h1>
-      <div class="sm:w-full flex items-center text-black mr-6">
+      <div class="w-full flex justify-center items-center text-black mr-6">
         <svg class="fill-current text-gray-800  h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 640 512" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"/><path fill="currentColor" d="M640 130.94V96c0-17.67-14.33-32-32-32H32C14.33 64 0 78.33 0 96v34.94c18.6 6.61 32 24.19 32 45.06s-13.4 38.45-32 45.06V320h640v-98.94c-18.6-6.61-32-24.19-32-45.06s13.4-38.45 32-45.06zM224 256h-64V128h64v128zm128 0h-64V128h64v128zm128 0h-64V128h64v128zM0 448h64v-26.67c0-8.84 7.16-16 16-16s16 7.16 16 16V448h128v-26.67c0-8.84 7.16-16 16-16s16 7.16 16 16V448h128v-26.67c0-8.84 7.16-16 16-16s16 7.16 16 16V448h128v-26.67c0-8.84 7.16-16 16-16s16 7.16 16 16V448h64v-96H0v96z" class=""></path></svg>
         <span class="font-semibold text-md text-gray-800 tracking-tight">%system_heap%</span>
+      </div>
+      <div class="w-full flex justify-center items-center text-black mr-6 mt-4">
+        <svg class="fill-current text-gray-800  h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 640 512" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"/><path fill="currentColor" d="M634.91 154.88C457.74-8.99 182.19-8.93 5.09 154.88c-6.66 6.16-6.79 16.59-.35 22.98l34.24 33.97c6.14 6.1 16.02 6.23 22.4.38 145.92-133.68 371.3-133.71 517.25 0 6.38 5.85 16.26 5.71 22.4-.38l34.24-33.97c6.43-6.39 6.3-16.82-.36-22.98zM320 352c-35.35 0-64 28.65-64 64s28.65 64 64 64 64-28.65 64-64-28.65-64-64-64zm202.67-83.59c-115.26-101.93-290.21-101.82-405.34 0-6.9 6.1-7.12 16.69-.57 23.15l34.44 33.99c6 5.92 15.66 6.32 22.05.8 83.95-72.57 209.74-72.41 293.49 0 6.39 5.52 16.05 5.13 22.05-.8l34.44-33.99c6.56-6.46 6.33-17.06-.56-23.15z" class=""></path></svg>
+        <span class="font-semibold text-md text-gray-800 tracking-tight">%wifi_ssid%</span>
       </div>
     </div>
     <a class="block text-center text-xs underline mt-6" href="/admin">Go to Admin</a>
@@ -222,6 +226,8 @@ String processor(const String& var){
   } else if(var == "system_heap") {
     String system_heap = String(ESP.getFreeHeap()) + " bytes";
     return system_heap;
+  } else if(var == "wifi_ssid") {
+    return WiFi.SSID();;
   }
   return String();
 }
